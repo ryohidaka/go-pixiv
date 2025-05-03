3efaa4f1d212fc9d8da6b431e738014c2ab09865
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-03 12:33:58 +0900

    feat(utils): Add parseNextPageOffset()

--- utils.go
+++ utils.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"strconv"
 	"time"
 )
 
@@ -76,4 +78,46 @@ func decodeJSON(body []byte, out any) error {
 //   - time.Time: The time when the access token will expire.
 func getExpiresAt(expiredIn int) time.Time {
 	return time.Now().Add(time.Duration(expiredIn) * time.Second)
+}
+
+// parseNextPageOffset extracts the offset value from the query parameters of a given URL string.
+//
+// Parameters:
+//   - s: A string representing the full URL (e.g., "https://example.com?page=2&offset=100").
+//   - field: The name of the query parameter to extract the offset from (e.g., "offset").
+//
+// Returns:
+//   - int: The extracted offset value. Returns 0 if the input URL is empty.
+//   - error: An error if the URL is invalid, the parameter is missing, or the value cannot be converted to an integer.
+func parseNextPageOffset(s, field string) (int, error) {
+	// If the input string is empty, return 0 as a default offset.
+	if s == "" {
+		return 0, nil
+	}
+
+	// Parse the input string into a URL structure.
+	u, err := url.Parse(s)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse URL: %s {%s}", s, err)
+	}
+
+	// Parse the raw query string from the URL.
+	queryParams, err := url.ParseQuery(u.RawQuery)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse query parameters: %s {%s}", s, err)
+	}
+
+	// Retrieve the offset value from the query parameters using the specified field name.
+	offsetParam := queryParams.Get(field)
+	if offsetParam == "" {
+		return 0, fmt.Errorf("missing query parameter: %s", field)
+	}
+
+	// Convert the offset parameter from string to integer.
+	offset, err := strconv.Atoi(offsetParam)
+	if err != nil {
+		return 0, fmt.Errorf("invalid offset value: %s {%s}", offsetParam, err)
+	}
+
+	return offset, nil
 }
