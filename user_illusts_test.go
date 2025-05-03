d52af83858df3ca0fba99224167355b6731f2839
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-03 14:14:34 +0900

    test(api): Add test for UserIllust()

---
+++ user_illusts_test.go
@@ -0,0 +1,45 @@
+package pixiv_test
+
+import (
+	"testing"
+
+	"github.com/ryohidaka/go-pixiv"
+	"github.com/ryohidaka/go-pixiv/models"
+	"github.com/ryohidaka/go-pixiv/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserIllust(t *testing.T) {
+	testutil.WithMockHTTP(t, func() {
+		// Mock the authentication response
+		_ = testutil.MockResponseFromFile("POST", pixiv.AuthHosts+"auth/token", "auth.json")
+
+		// Mock the user illusts response
+		url := pixiv.AppHosts + "v1/user/illusts?filter=for_ios&user_id=11"
+		err := testutil.MockResponseFromFile("GET", url, "user-illusts.json")
+		assert.NoError(t, err)
+
+		// Initialize the AppPixivAPI instance
+		api, err := pixiv.NewApp("dummy-refresh-token")
+		assert.NoError(t, err)
+
+		// Prepare options
+		filter := "for_ios"
+		opts := &pixiv.UserIllustsOptions{
+			Filter: &filter,
+		}
+
+		// Call the UserIllusts method
+		illusts, next, err := api.UserIllusts(11, opts)
+		assert.NoError(t, err)
+		assert.Len(t, illusts, 1)
+		assert.Equal(t, 30, next)
+
+		// Check contents of the first illustration
+		illust := illusts[0]
+		assert.Equal(t, uint64(129899459), illust.ID)
+		assert.Equal(t, "「出張版！アクション月例漫画賞」いよいよ開催！", illust.Title)
+		assert.Equal(t, models.IllustType("illust"), models.IllustType(illust.Type))
+		assert.Equal(t, "https://i.pximg.net/c/600x1200_90/img-master/img/2025/05/01/11/19/11/129899459_p0_master1200.jpg", illust.ImageURLs.Large)
+	})
+}
