85f8cbff25b9c7939814d9a6fe3d00b176ee36d1
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-04 15:16:17 +0900

    feat(api): Add IllustBookmarkDetail()

---
+++ illust_bookmark_detail.go
@@ -0,0 +1,38 @@
+package pixiv
+
+import "github.com/ryohidaka/go-pixiv/models"
+
+// illustBookmarkDetailParams represents the request parameters
+// for retrieving illustration bookmark detail from the Pixiv API.
+type illustBookmarkDetailParams struct {
+	IllustID uint64 `url:"illust_id,omitempty"` // The ID of the illustration
+}
+
+// IllustBookmarkDetail retrieves the bookmark detail information
+// for a specific illustration by its ID.
+//
+// Parameters:
+//   - id: The illustration ID to fetch the bookmark details for.
+//
+// Returns:
+//   - A pointer to IllustBookmarkDetail containing the bookmark info.
+//   - An error if the API request fails.
+func (a *AppPixivAPI) IllustBookmarkDetail(id uint64) (*models.IllustBookmarkDetail, error) {
+	path := "v2/illust/bookmark/detail"
+
+	// Construct request parameters
+	params := &illustBookmarkDetailParams{
+		IllustID: id, // Set the illustration ID parameter
+	}
+
+	// Initialize the response model
+	data := &models.IllustBookmarkDetail{}
+
+	// Perform the API request with the specified path and parameters
+	if err := a.Request(path, params, data); err != nil {
+		return nil, err
+	}
+
+	// Return the illustration bookmark detail data from the response
+	return data, nil
+}
---
+++ models/illust_bookmark.go
@@ -0,0 +1,16 @@
+package models
+
+type IllustBookmarkDetail struct {
+	BookmarkDetail BookmarkDetail `json:"bookmark_detail"`
+}
+
+type BookmarkDetail struct {
+	IsBookmarked bool                `json:"is_bookmarked"`
+	Tags         []BookmarkDetailTag `json:"tags"`
+	Restrict     string              `json:"restrict"`
+}
+
+type BookmarkDetailTag struct {
+	Name         string `json:"name"`
+	IsRegistered bool   `json:"is_registered"`
+}
