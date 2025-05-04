dd4f401911770f930caa3933cb4b6c051dc8bd5b
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-04 23:21:24 +0900

    test(api): Add test for FetchAllBookmarkedIllusts()

--- user_bookmarks_illust_test.go
+++ user_bookmarks_illust_test.go
@@ -62,3 +62,43 @@ func TestUserBookmarksIllust(t *testing.T) {
 		assert.Equal(t, "https://i.pximg.net/c/600x1200_90/img-master/img/2025/05/01/11/19/11/129899459_p0_master1200.jpg", illust.ImageURLs.Large)
 	})
 }
+
+func TestFetchAllBookmarkedIllusts(t *testing.T) {
+	testutil.WithMockHTTP(t, func() {
+		// Mock: authentication response
+		_ = testutil.MockResponseFromFile("POST", pixiv.AuthHosts+"auth/token", "auth/token")
+
+		// Mock: page 1 of user bookmarks
+		urlPage1 := pixiv.AppHosts + "v1/user/bookmarks/illust?filter=for_ios&restrict=public&user_id=11"
+		err := testutil.MockResponseFromFile("GET", urlPage1, "v1/user/bookmarks/illust")
+		assert.NoError(t, err)
+
+		// Mock: page 2 of user bookmarks (with next_id)
+		urlPage2 := pixiv.AppHosts + "v1/user/bookmarks/illust?filter=for_ios&max_bookmark_id=129899459&restrict=public&user_id=11"
+		err = testutil.MockResponseFromFile("GET", urlPage2, "v1/user/bookmarks/illust_end")
+		assert.NoError(t, err)
+
+		// Initialize API instance
+		api, err := pixiv.NewApp("dummy-refresh-token")
+		assert.NoError(t, err)
+
+		// Set request options
+		filter := "for_ios"
+		public := models.Public
+		opts := &pixiv.UserBookmarksIllustOptions{
+			Filter:   &filter,
+			Restrict: &public,
+		}
+
+		// Call the main function (no sleep between requests)
+		illusts, err := api.FetchAllBookmarkedIllusts(11, opts, 0)
+		assert.NoError(t, err)
+		assert.Len(t, illusts, 2) // One illustration per page
+
+		// Verify contents of each illustration
+		assert.Equal(t, uint64(129899459), illusts[0].ID)
+		assert.Equal(t, uint64(129899459), illusts[1].ID)
+		assert.Equal(t, "「出張版！アクション月例漫画賞」いよいよ開催！", illusts[0].Title)
+		assert.Equal(t, "「出張版！アクション月例漫画賞」いよいよ開催！", illusts[1].Title)
+	})
+}
