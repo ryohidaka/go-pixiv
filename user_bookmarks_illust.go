e327c71a50f3ff88e10c55bd12aaf58a4ae4c54e
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-04 23:20:58 +0900

    feat(api): Add FetchAllBookmarkedIllusts()

--- user_bookmarks_illust.go
+++ user_bookmarks_illust.go
@@ -1,6 +1,12 @@
 package pixiv
 
-import "github.com/ryohidaka/go-pixiv/models"
+import (
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/ryohidaka/go-pixiv/models"
+)
 
 // UserBookmarksIllustOptions defines optional parameters for retrieving user bookmarks illust.
 type UserBookmarksIllustOptions struct {
@@ -57,4 +63,68 @@ func (a *AppPixivAPI) UserBookmarksIllust(uid uint64, opts *UserBookmarksIllustO
 	// Parse the next page offset from the NextURL (if any)
 	next, err := parseNextPageOffset(data.NextURL, "max_bookmark_id")
 	return data.Illusts, next, err
+}
+
+// FetchAllBookmarkedIllusts retrieves all bookmarked illustrations for a given user by paginating
+//
+// Parameters:
+//   - uid: Pixiv user ID to retrieve bookmarks for.
+//   - baseOpts: Optional filter parameters (Restrict, Filter, Tag). MaxBookmarkID will be overwritten for pagination.
+//   - sleepMs: Optional sleep duration between requests in milliseconds (default: 1000ms).
+//
+// Returns:
+//   - []models.Illust: A combined list of all retrieved bookmarked illustrations (even if an error occurs mid-fetch).
+//   - error: Any error encountered during the API request.
+func (a *AppPixivAPI) FetchAllBookmarkedIllusts(uid uint64, opts *UserBookmarksIllustOptions, sleepMs ...int) ([]models.Illust, error) {
+	var allIllusts []models.Illust
+	var next int
+	var err error
+
+	// Use default sleep duration of 1000ms unless explicitly specified
+	sleepDuration := 1000 * time.Millisecond
+	if len(sleepMs) > 0 {
+		sleepDuration = time.Duration(sleepMs[0]) * time.Millisecond
+	}
+
+	// Initialize the slog logger with options
+	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{})
+	logger := slog.New(handler)
+
+	// Start the fetch process
+	logger.Info("Fetching bookmarked illustrations", "uid", uid)
+
+	for {
+		// Retrieve a page of bookmarked illustrations
+		var illusts []models.Illust
+		illusts, next, err = a.UserBookmarksIllust(uid, opts)
+
+		// Log the number of illustrations fetched in this request
+		logger.Info("Fetched illustrations", "count", len(illusts), "nextBookmarkID", next)
+
+		// Always append the successfully fetched results before returning on error
+		allIllusts = append(allIllusts, illusts...)
+		if err != nil {
+			// Log the error
+			logger.Error("Error fetching illustrations", "error", err)
+			return allIllusts, err
+		}
+
+		// Exit loop if there are no more pages
+		if next == 0 {
+			logger.Info("No more pages to fetch, exiting")
+			break
+		}
+
+		// Set the next max_bookmark_id for pagination
+		opts.MaxBookmarkID = &next
+
+		// Sleep between requests to avoid rate limits
+		logger.Info("Sleeping before next request", "sleepDuration", sleepDuration)
+		time.Sleep(sleepDuration)
+	}
+
+	// Log the total number of illustrations fetched
+	logger.Info("Total illustrations fetched", "total", len(allIllusts))
+
+	return allIllusts, nil
 }
