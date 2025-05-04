ca3723d9c2e3726d5f64ead1c8b4a1ae58572163
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-04 23:13:42 +0900

    test(api): Add test for FetchAllUserIllusts()

--- user_illusts_test.go
+++ user_illusts_test.go
@@ -60,3 +60,41 @@ func TestUserIllust(t *testing.T) {
 		assert.Equal(t, "https://i.pximg.net/c/600x1200_90/img-master/img/2025/05/01/11/19/11/129899459_p0_master1200.jpg", illust.ImageURLs.Large)
 	})
 }
+
+func TestFetchAllUserIllusts(t *testing.T) {
+	testutil.WithMockHTTP(t, func() {
+		// Mock: authentication response
+		_ = testutil.MockResponseFromFile("POST", pixiv.AuthHosts+"auth/token", "auth/token")
+
+		// Mock: page 1 of user illustrations
+		urlPage1 := pixiv.AppHosts + "v1/user/illusts?filter=for_ios&user_id=11"
+		err := testutil.MockResponseFromFile("GET", urlPage1, "v1/user/illusts")
+		assert.NoError(t, err)
+
+		// Mock: page 2 of user illustrations (with offset)
+		urlPage2 := pixiv.AppHosts + "v1/user/illusts?filter=for_ios&offset=30&user_id=11"
+		err = testutil.MockResponseFromFile("GET", urlPage2, "v1/user/illusts_end")
+		assert.NoError(t, err)
+
+		// Initialize API instance
+		api, err := pixiv.NewApp("dummy-refresh-token")
+		assert.NoError(t, err)
+
+		// Set request options
+		filter := "for_ios"
+		opts := &pixiv.UserIllustsOptions{
+			Filter: &filter,
+		}
+
+		// Call the main function (no sleep between requests)
+		illusts, err := api.FetchAllUserIllusts(11, opts, 0)
+		assert.NoError(t, err)
+		assert.Len(t, illusts, 2) // One illustration per page
+
+		// Verify contents of each illustration
+		assert.Equal(t, uint64(129899459), illusts[0].ID)
+		assert.Equal(t, uint64(129899459), illusts[1].ID)
+		assert.Equal(t, "「出張版！アクション月例漫画賞」いよいよ開催！", illusts[0].Title)
+		assert.Equal(t, "「出張版！アクション月例漫画賞」いよいよ開催！", illusts[1].Title)
+	})
+}
