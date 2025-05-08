39e2caeef018367c77ad06b952cd7e755a6c15fb
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-08 21:21:01 +0900

    fix(models): Add missing fields for User struct.

--- models/user.go
+++ models/user.go
@@ -1,12 +1,14 @@
 package models
 
 type User struct {
-	ID            uint64      `json:"id"`
-	Name          string      `json:"name"`
-	Account       string      `json:"account"`
-	ProfileImages *UserImages `json:"profile_image_urls"`
-	Comment       *string     `json:"comment,omitempty"` // `Comment` is used only in UserDetail
-	IsFollowed    bool        `json:"is_followed"`
+	ID                   uint64      `json:"id"`
+	Name                 string      `json:"name"`
+	Account              string      `json:"account"`
+	ProfileImages        *UserImages `json:"profile_image_urls"`
+	Comment              *string     `json:"comment,omitempty"` // `Comment` is used only in UserDetail
+	IsFollowed           bool        `json:"is_followed"`
+	IsAccessBlockingUser *bool       `json:"is_access_blocking_user,omitempty"` // `IsAccessBlockingUser` is used only in UserDetail
+	IsAcceptRequest      *bool       `json:"is_accept_request,omitempty"`       // `IsAcceptRequest` is used only in UserFollowing and UserFollower
 }
 
 type UserImages struct {
