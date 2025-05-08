dc8611de6c4d62009c4d67e5dcd13c9cf83e8d04
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-08 21:49:07 +0900

    fix(models): Add missing fields for Illust struct.

--- models/illust.go
+++ models/illust.go
@@ -12,31 +12,33 @@ type IllustResponse struct {
 }
 
 type Illust struct {
-	ID             uint64          `json:"id"`
-	Title          string          `json:"title"`
-	Type           IllustType      `json:"type"`
-	ImageURLs      *Images         `json:"image_urls"`
-	Caption        string          `json:"caption"`
-	Restrict       int             `json:"restrict"`
-	User           *User           `json:"user"`
-	Tags           []Tag           `json:"tags"`
-	Tools          []string        `json:"tools"`
-	CreateDate     time.Time       `json:"create_date"`
-	PageCount      int             `json:"page_count"`
-	Width          int             `json:"width"`
-	Height         int             `json:"height"`
-	SanityLevel    int             `json:"sanity_level"`
-	XRestrict      int             `json:"x_restrict"`
-	Series         *Series         `json:"series"`
-	MetaSinglePage *MetaSinglePage `json:"meta_single_page"`
-	MetaPages      []MetaPage      `json:"meta_pages"`
-	TotalView      int             `json:"total_view"`
-	TotalBookmarks int             `json:"total_bookmarks"`
-	IsBookmarked   bool            `json:"is_bookmarked"`
-	Visible        bool            `json:"visible"`
-	IsMuted        bool            `json:"is_muted"`
-	TotalComments  int             `json:"total_comments"`
-	IllustAIType   IllustAIType    `json:"illust_ai_type"`
+	ID                   uint64          `json:"id"`
+	Title                string          `json:"title"`
+	Type                 IllustType      `json:"type"`
+	ImageURLs            *Images         `json:"image_urls"`
+	Caption              string          `json:"caption"`
+	Restrict             int             `json:"restrict"`
+	User                 *User           `json:"user"`
+	Tags                 []Tag           `json:"tags"`
+	Tools                []string        `json:"tools"`
+	CreateDate           time.Time       `json:"create_date"`
+	PageCount            int             `json:"page_count"`
+	Width                int             `json:"width"`
+	Height               int             `json:"height"`
+	SanityLevel          int             `json:"sanity_level"`
+	XRestrict            int             `json:"x_restrict"`
+	Series               *Series         `json:"series"`
+	MetaSinglePage       *MetaSinglePage `json:"meta_single_page"`
+	MetaPages            []MetaPage      `json:"meta_pages"`
+	TotalView            int             `json:"total_view"`
+	TotalBookmarks       int             `json:"total_bookmarks"`
+	IsBookmarked         bool            `json:"is_bookmarked"`
+	Visible              bool            `json:"visible"`
+	IsMuted              bool            `json:"is_muted"`
+	TotalComments        int             `json:"total_comments"`
+	IllustAIType         IllustAIType    `json:"illust_ai_type"`
+	IllustBookStyle      int             `json:"illust_book_style"`
+	CommentAccessControl *int            `json:"comment_access_control"` // `CommentAccessControl` is used only in IllustDetail
 }
 
 type Images struct {
