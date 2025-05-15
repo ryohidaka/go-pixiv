aada057616399d9bb2eca716efea0ce990192e83
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-05-15 23:22:31 +0900

    test(crawler): Add test for NewCrawler()

---
+++ crawler/crawler_test.go
@@ -0,0 +1,24 @@
+package crawler_test
+
+import (
+	"testing"
+
+	"github.com/ryohidaka/go-pixiv"
+	"github.com/ryohidaka/go-pixiv/crawler"
+	"github.com/ryohidaka/go-pixiv/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestNewApp verifies that NewApp correctly initializes the API with mocked authentication.
+func TestNewCrawler(t *testing.T) {
+	testutil.WithMockHTTP(t, func() {
+		// Mock the authentication response
+		err := testutil.MockResponseFromFile("POST", pixiv.AuthHosts+"auth/token", "auth/token", true)
+		assert.NoError(t, err)
+
+		// Create a new Pixiv Crawler
+		c, _ := crawler.NewCrawler("dummy-refresh-token")
+		assert.NoError(t, err)
+		assert.NotNil(t, c)
+	})
+}
