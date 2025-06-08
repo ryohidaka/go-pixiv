1196e2f7b095e562e2da7a4584be97b9d491b619
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-06-08 23:05:07 +0900

    test(api): Add test for UserBookmarksIllusts()

---
+++ pkg/webapi/user_bookmarked_illusts_test.go
@@ -0,0 +1,59 @@
+package webapi_test
+
+import (
+	"testing"
+
+	"github.com/ryohidaka/go-pixiv"
+	"github.com/ryohidaka/go-pixiv/models/webmodel/core"
+	"github.com/ryohidaka/go-pixiv/pkg/webapi"
+	"github.com/ryohidaka/go-pixiv/testutil/webtest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserBookmarksIllusts(t *testing.T) {
+	webtest.WithMockHTTP(t, func() {
+		// Mock the user following response
+		url := webapi.ApiHosts + "user/11/illusts/bookmarks?limit=3&rest=show"
+		err := webtest.MockResponseFromFile("GET", url, "user_bookmarked_illusts")
+		assert.NoError(t, err)
+
+		// Initialize the WebPixivAPI instance
+		app, err := pixiv.NewWebApp("dummy-phpsessid")
+		assert.NoError(t, err)
+
+		// Call the UserBookmarksIllusts method
+		opts := pixiv.UserBookmarksIllustsOptions{
+			Offset:   0,
+			Limit:    3,
+			Restrict: core.Show,
+		}
+
+		data, total, err := app.UserBookmarksIllusts(11, opts)
+		assert.NoError(t, err)
+		assert.NotNil(t, data)
+
+		// Check total
+		assert.Equal(t, 3, int(total))
+
+		expected := []struct {
+			id    string
+			title string
+			tag   string
+		}{
+			{"130967689", "comic POOL5月コミックス発売情報", "漫画"},
+			{"131093702", "マンガ投稿企画『ツキコミ〜裏を見ちゃった！〜』開催", "漫画"},
+			{"131202453", "pixivが大阪・関西万博で期間限定展示を開催", "公式企画"},
+		}
+
+		for i, exp := range expected {
+			assert.Equal(t, exp.id, data.Works[i].ID)
+			assert.Equal(t, exp.title, data.Works[i].Title)
+
+			tags, ok := data.BookmarkTags[exp.id]
+			assert.True(t, ok, "bookmark tags must exist for id "+exp.id)
+			assert.Greater(t, len(tags), 0)
+			assert.Equal(t, exp.tag, tags[0])
+		}
+
+	})
+}
